df5c6423083f3011cac51c2235488be242a192da
thatsMind <collinzizhen@gmail.com>
2022-03-23 13:57:45 -0700

    implement operate function

--- javascript.js
+++ javascript.js
@@ -13,3 +13,22 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1/num2;
 }
+
+function operate(num1, num2, operator){
+    switch(operator){
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+            alert("wrong operator")
+    }
+}
