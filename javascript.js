fa1f18035ce3dfab7493f94dfd1447a75bbf2498
thatsMind <collinzizhen@gmail.com>
2022-03-24 15:21:21 -0700

    fix logical bugs, finished product 1.0, and added css.

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ function add(num1, num2){
 }
 
 function subtract(num1, num2){
-    return num1 - num2;
+    return num1-num2;
 }
 
 function multiply(num1, num2){
@@ -31,40 +31,62 @@ function operate(num1, num2, operator){
 
 const container = document.querySelector(".container")
 const displayValue = document.querySelector(".display")
-let visualInput="";
-let tempVisual="0";
-displayValue.textContent=tempVisual;
+
+let userInput="";
+let visualResult="0";
+displayValue.textContent=visualResult;
 let num1="";
 let num2="";
 let operator="";
+let justOperated=false;
 
-
+//This should be ok
 container.addEventListener("click", (e)=>{
     const isButton = e.target.className == "number";
     if(!isButton){
         return;
     }
-    visualInput+=e.target.innerText;
-    displayValue.textContent=visualInput;
+    userInput+=e.target.innerText;
+    displayValue.textContent=userInput;
     log()
 })
+
 
 container.addEventListener("click", (e)=>{
     const isButton = e.target.className == "operator";
     if(!isButton){
         return;
     }
-    if(operator!=""&&num1!=""){
-        num2=visualInput;
-        visualInput=operate(num1,num2,operator);
-        displayValue.textContent=visualInput;
+    if (justOperated){
+        log("1 OPERATOR justOperated")
+
+        if(userInput!=""){
+            log("2 OPERATOR justOperated")
+
+            num1=userInput;
+        }
+        justOperated=false
     }
-    num1=visualInput;
+
+    if(num2==""&&!(num1==="")&&operator!=""&&userInput!=""){
+        log("3 OPERATOR INSIDE MULTIPLE CONDITION")
+
+        num2=userInput; //pre operate function
+        userInput=operate(num1,num2,operator);
+        displayValue.textContent=userInput;
+        num1=userInput //after operate function
+        log("3 OPERATOR INSIDE MULTIPLE CONDITION AFTER CALCULATION")
+
+    }
+    if (num1===""){
+        log("4 OPERATOR num1==")
+        num1=userInput;
+    }
     operator=e.target.innerText;
-    tempVisual=visualInput;
-    visualInput="";
-    displayValue.textContent=tempVisual;
-    log()
+    log("5 OPERATOR")
+    num2=""
+    userInput="";
+    log("6 OPERATOR RESET num2 and userInput")
 
 })
 
@@ -73,38 +95,40 @@ container.addEventListener("click", (e)=>{
     if(!isButton){
         return;
     }
-    num2=visualInput;
-    tempVisual
-    visualInput=operate(num1,num2,operator);
-    displayValue.textContent=visualInput;
-    visualInput=""
-    num1="";
+    num2=userInput;
+    visualResult=operate(num1,num2,operator);
+    displayValue.textContent=visualResult;
+
+    justOperated=true;
+    num1=visualResult;
     num2="";
+    userInput=""
     operator="";
     log()
 
 })
 
+//This should be ok
 container.addEventListener("click", (e)=>{
     const isButton = e.target.className == "clear";
     if(!isButton){
         return;
     }
-    visualInput="";
-    tempVisual="";
+    userInput="";
+    visualResult="0";
     num1="";
     num2="";
     operator="";
-    displayValue.textContent=visualInput;
+    displayValue.textContent=visualResult;
     log()
 
 })
-function log(){
-    console.log(`visualInput: ${visualInput}`)
-    console.log(`tempVisual:${tempVisual}`)
+function log(something){
+    console.log(something)
+    console.log(`userInput: ${userInput}`)
     console.log(`num1:${num1}`)
-    console.log(`num2:${num2}`)
     console.log(`operator:${operator}`)
+    console.log(`num2:${num2}`)
+    console.log(`visualResult:${visualResult}`)
     console.log(`---------------------`)
-
 }
